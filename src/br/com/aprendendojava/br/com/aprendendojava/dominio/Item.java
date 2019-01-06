62bbe375a1dea6b44eae86298ec4cab8d7cd9f7f
Joseph <josephmugiwara@gmail.com>
2019-01-05 22:22:39 -0200

    Getters e Setters.

--- src/br/com/aprendendojava/br/com/aprendendojava/dominio/Item.java
+++ src/br/com/aprendendojava/br/com/aprendendojava/dominio/Item.java
@@ -1,4 +1,28 @@
 package br.com.aprendendojava.br.com.aprendendojava.dominio;
 
 public class Item {
+
+    private String nome;
+    private int anoCriacao;
+
+    public Item(String nome, int anoCriacao){
+        this.nome = nome;
+        this.anoCriacao = anoCriacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoCriacao() {
+        return anoCriacao;
+    }
+
+    public void setAnoCriacao(int anoCriacao) {
+        this.anoCriacao = anoCriacao;
+    }
 }
